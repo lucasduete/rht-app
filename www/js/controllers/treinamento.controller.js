3f88131d7611ff5062d2a52e439985f42831f3aa
Lucas Duete <lm.duete@gmail.com>
2019-03-01 19:56:00 -0300

    Criando funçao de listagem de todos os treinemntos que e inicializada sozinha para melhor entendimento do codigo

--- www/js/controllers/treinamento.controller.js
+++ www/js/controllers/treinamento.controller.js
@@ -3,51 +3,53 @@ controllersManager.controller('treinamentoController', function ($scope, $rootSc
     $scope.trainings = [];
     $scope.training = {};
 
-    var request = {
-        method: 'GET',
-        url: 'http://localhost:8080/training/',
-    }
-
-    $http(request).then(function (response) {
-
-        // Atualiza os treinamentos no escopo
-        $scope.trainings = response.data;
-        
-        // Salva os treinametos no LocalStorage
-        var data = angular.toJson($scope.trainings);
-        localStorage.setItem("trainings", data);
-
-        console.log(response.data);
-
-    }, function (err) {
-        console.log(err.data);
-        
-        if (err.status == 401) {
-            //alerta de erro
-            var alertPopup = $ionicPopup.alert({
-                title: 'Erro!',
-                template: 'Login ou senha inválida!'
-            });
-        } else if (err.status == 400) {
-            //alerta de erro
-            var alertPopup = $ionicPopup.alert({
-                title: 'Erro!',
-                template: 'Campo em branco, informe o login e a senha!'
-            });
-        } else if (err.status == 500) {
-            //alerta de erro
-            var alertPopup = $ionicPopup.alert({
-                title: 'Erro!',
-                template: 'Servidor indisponível, tente novamente mais tarde'
-            });
-        } else {
-            //alerta de erro
-            var alertPopup = $ionicPopup.alert({
-                title: 'Erro!',
-                template: 'Não foi possível efetuar o login!'
-            });
+    $scope.listAll = function () {
+        var request = {
+            method: 'GET',
+            url: 'http://localhost:8080/training/',
         }
-    });
+    
+        $http(request).then(function (response) {
+    
+            // Atualiza os treinamentos no escopo
+            $scope.trainings = response.data;
+            
+            // Salva os treinametos no LocalStorage
+            var data = angular.toJson($scope.trainings);
+            localStorage.setItem("trainings", data);
+    
+            console.log(response.data);
+    
+        }, function (err) {
+            console.log(err.data);
+            
+            if (err.status == 401) {
+                //alerta de erro
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Erro!',
+                    template: 'Login ou senha inválida!'
+                });
+            } else if (err.status == 400) {
+                //alerta de erro
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Erro!',
+                    template: 'Campo em branco, informe o login e a senha!'
+                });
+            } else if (err.status == 500) {
+                //alerta de erro
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Erro!',
+                    template: 'Servidor indisponível, tente novamente mais tarde'
+                });
+            } else {
+                //alerta de erro
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Erro!',
+                    template: 'Não foi possível efetuar o login!'
+                });
+            }
+        });
+    }()
 
     $scope.abrir = function (treinamentoId) {
 
