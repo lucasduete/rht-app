d7554c5845595bb5c63962df5969707828e2250f
Lucas Duete <lm.duete@gmail.com>
2019-03-12 17:22:37 -0300

    Implementada função no controllador de TreinamentoDetail para enviar uma nota para um treinamento, refatorada tela detalhesTreinamento.html para que um Employee possa enviar uma nota.

--- www/js/controllers/treinamento.detail.controller.js
+++ www/js/controllers/treinamento.detail.controller.js
@@ -1,7 +1,6 @@
 controllersManager.controller('treinamentoDetailController', function ($scope, $rootScope, $http, $state, $ionicPopup, $ionicLoading, $ionicHistory) {
 
     $scope.trainings = [];
-
     $scope.treinamento = {};
 
     $scope.retrieve = function () {
@@ -35,6 +34,70 @@ controllersManager.controller('treinamentoDetailController', function ($scope, $
             return false;
         }
     };
+
+    $scope.enviarNota = function(ratingPoints, idTraining) {
+
+        var user = JSON.parse(localStorage.getItem("user"));
+        var emailEmployee = user.email;
+
+        var request = {
+            method: 'POST',
+			url: 'http://localhost:8080/rating/',
+			data: 'idTraining=' + idTraining + '&emailEmployee=' + emailEmployee + '&points=' + ratingPoints,
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+        }
+        
+        $http(request).then(function (response) {
+
+            console.log($scope.treinamento);
+
+            var alertPopup = $ionicPopup.alert({
+                title: 'Sucess!',
+                template: 'Avaliação Enviada com Sucesso!',
+                buttons: [
+                    {
+                        text: "<b>OK!<b>",
+                        type: 'button-positive',
+                        onTap: function(e) {
+                            // Não faz nada
+                            // Para recarregar a pagina: location.reload();
+                        }
+                    }
+                ]
+            });
+
+        }, function (err) {
+            console.log(err.data);
+            
+            if (err.status == 400) {
+                //alerta de erro
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Erro!',
+                    template: 'Verifique os dados e tente novamente!'
+                });
+            } else if (err.status == 304) {
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Erro!',
+                    template: 'Você já enviou sua avaliação!'
+                });
+            } else if (err.status == 500) {
+                //alerta de erro
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Erro!',
+                    template: 'Servidor indisponível, tente novamente mais tarde'
+                });
+            } else {
+                //alerta de erro
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Erro!',
+                    template: 'Não foi possível enviar sua nota, por favor, tente novamente!'
+                });
+            }
+        });
+
+    }
 
     $scope.cadastrar = function () {
         
