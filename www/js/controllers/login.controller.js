a1081c4801d35d21750a7aa729a204cc5c93a1ab
Lucas Duete <lm.duete@gmail.com>
2019-03-11 21:43:25 -0300

    Adicionando login como employee

--- www/js/controllers/login.controller.js
+++ www/js/controllers/login.controller.js
@@ -3,7 +3,7 @@ controllersManager.controller('loginController', function ($scope, $rootScope, $
     $scope.user = {};
 	$scope.login = function (credenciais) {
 
-		var request = {
+		var requestLoginAppraiser = {
 			method: 'POST',
 			url: 'http://localhost:8080/appraiser/login',
 			data: 'email=' + credenciais.email + '&password=' + credenciais.senha,
@@ -12,10 +12,20 @@ controllersManager.controller('loginController', function ($scope, $rootScope, $
 			}
 		}
 
-		$http(request).then(function (response) {
+		var requestLoginEmployee = {
+			method: 'POST',
+			url: 'http://localhost:8080/employee/login',
+			data: 'email=' + credenciais.email + '&password=' + credenciais.senha,
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+		}
 
-			// Atualiza usuario no escopo
+		$http(requestLoginAppraiser).then(function (response) {
+
+			// Atualiza usuario e tipo de usuario no escopo
 			$scope.user = response.data;
+			$scope.typeUser = "appraiser";
 			
 			// Salva usuario no LocalStorage
 			var data = angular.toJson($scope.aluno);
@@ -30,11 +40,52 @@ controllersManager.controller('loginController', function ($scope, $rootScope, $
 			console.log(err.data);
 			
 			if (err.status == 401) {
-				//alerta de erro
-				var alertPopup = $ionicPopup.alert({
-					title: 'Erro!',
-					template: 'Login ou senha inválida!'
+				
+				$http(requestLoginEmployee).then(function (response) {
+
+					// Atualiza usuario e tipo de usuario no escopo
+					$scope.user = response.data;
+					$scope.typeUser = "employee";
+					
+					// Salva usuario no LocalStorage
+					var data = angular.toJson($scope.aluno);
+					localStorage.setItem("user", data);
+		
+					console.log(response.data);
+					
+					// Redireciona para página inical
+					$state.go('menu.home');
+		
+				}, function (err) {
+					console.log(err.data);
+					
+					if (err.status == 401) {
+						//alerta de erro
+						var alertPopup = $ionicPopup.alert({
+							title: 'Erro!',
+							template: 'Login ou senha inválida!'
+						});
+					} else if (err.status == 400) {
+						//alerta de erro
+						var alertPopup = $ionicPopup.alert({
+							title: 'Erro!',
+							template: 'Campo em branco, informe o login e a senha!'
+						});
+					} else if (err.status == 500) {
+						//alerta de erro
+						var alertPopup = $ionicPopup.alert({
+							title: 'Erro!',
+							template: 'Servidor indisponível, tente novamente mais tarde'
+						});
+					} else {
+						//alerta de erro
+						var alertPopup = $ionicPopup.alert({
+							title: 'Erro!',
+							template: 'Não foi possível efetuar o login!'
+						});
+					}
 				});
+
 			} else if (err.status == 400) {
 				//alerta de erro
 				var alertPopup = $ionicPopup.alert({
